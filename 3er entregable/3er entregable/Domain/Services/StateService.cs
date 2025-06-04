b516fe48ca0091071a0ab91b58b86e72ec17b8c6
Juanmax521 <Juanmax521@gmail.com>
2025-06-03 22:18:56 -0500

    IStateService and StateService created, CountryService (EditCountryAsync) updated

--- 3er entregable/3er entregable/Domain/Services/StateService.cs
+++ 3er entregable/3er entregable/Domain/Services/StateService.cs
@@ -5,92 +5,94 @@ using Microsoft.EntityFrameworkCore;
 
 namespace _3er_entregable.Domain.Services
 {
-    public class CountryService : ICountryService
+    public class StateService : IStateService
     {
         private readonly DataBaseContext _context;
 
-        public CountryService(DataBaseContext context)
+        public StateService(DataBaseContext context)
         {
             _context = context;
         }
-        public async Task<IEnumerable<Country>> GetCountriesAsync()
+        public async Task<IEnumerable<State>> GetStateByCountryAsync(Guid id)
         {
             // Otra forma de hacerlo es
             // return await _context.Countries.ToListAsync();
 
             try
             {
-                var contries = await _context.Countries.ToListAsync();
-                return contries;
+                var states = await _context.States.Where(s => s.CountryId == id).ToListAsync(); // Obtiene todos los estados que pertenecen al país con el ID especificado
+                return states;
             }
             catch (DbUpdateException dbUpdateException)
             {
                 throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
             }
         }
-        public async Task<Country> GetCountryByIdAsync(Guid id)
+        public async Task<State> GetStateByIdAsync(Guid id)
         {
             try
             {
-                var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
-                return country;
-                // Otra forma de hacerlo es var country = await _context.Countries.FindAsync(id);
-                // otra mas es var country = await _context.Countries.FirstAsync(c => c.Id == id);
+                var states = await _context.States.FirstOrDefaultAsync(s => s.Id == id); // Obtiene un estado por su ID
+                return states;
             }
             catch (DbUpdateException dbUpdateException)
             {
                 throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
             }
         }
-
-        public async Task<Country> CreateCountryAsync(Country country)
+        public async Task<State> CreateStateAsync(State state)
         {
             try
             {
-                country.Id = Guid.NewGuid(); // Genera un nuevo ID para el país
-                country.CreatedDate = DateTime.Now;
-                _context.Countries.Add(country); // El metodo Add() me permite crear el objeto en el contexto de la BD
+                state.CreatedDate = DateTime.Now; // Asigna la fecha de creación al estado
+                state.Id = Guid.NewGuid(); // Genera un nuevo ID para el estado
+                state.Equals(state.CountryId); // Asegura que el estado esté asociado al país correcto
+                _context.States.Add(state); // Agrega el estado al contexto de la base de datos
                 await _context.SaveChangesAsync(); // Guarda los cambios en la base de datos
-                return country; // Devuelve el país creado
-            }
-            catch (DbUpdateException dbUpdateException)
-            {
-                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
-            }
+                return state; // Devuelve el estado creado
 
-        }
-        public async Task<Country> EditCountryAsync(Country country)
-        {
-            try
-            {
-                country.CreatedDate = country.CreatedDate; // Mantiene la fecha de creación
-                country.ModifiedDate = DateTime.Now; // Actualiza la fecha de modificación
-                _context.Countries.Update(country); // Virtualizo mi objeto
-                await _context.SaveChangesAsync(); // Guarda los cambios en la base de datos
-                return country; // Devuelve el país actualizado
             }
             catch (DbUpdateException dbUpdateException)
             {
                 throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
             }
         }
-        public async Task<Country> DeleteCountryAsync(Guid id)
+        public async Task<State> EditStateAsync(State state)
         {
             try
             {
-                var country = await GetCountryByIdAsync(id);
-                if (country == null)
+                state.CreatedDate = state.CreatedDate; // Mantiene la fecha de creación original
+                state.ModifiedDate = DateTime.Now; // Actualiza la fecha de modificación al momento actual
+                _context.States.Update(state); // Actualiza el estado en el contexto de la base de datos
+                await _context.SaveChangesAsync(); // Guarda los cambios en la base de datos
+                return state; // Devuelve el estado actualizado
+
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+        public async Task<State> DeleteStateAsync(Guid id)
+        {
+            try
+            {
+                var state = await GetStateByIdAsync(id);
+                if(state == null)
                 {
-                    return  null; // Si el país no existe, devuelve null
+                    return null; // Si el estado no existe, retorna null
                 }
-                _context.Countries.Remove(country); // Elimina el país de la base de datos
+                _context.States.Remove(state); // Elimina el estado del contexto de la base de datos
                 await _context.SaveChangesAsync(); // Guarda los cambios en la base de datos
-                return country; // Devuelve el país eliminado
+                return state; // Devuelve el estado eliminado
+
             }
             catch (DbUpdateException dbUpdateException)
             {
                 throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
             }
         }
+
+        
     }
 }
